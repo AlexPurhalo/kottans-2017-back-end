28c95a70de179fe436b4c6f626f5f0c7fb897f75
AlexPurhalo <alexpurhalo@gmail.com>
2017-02-25 18:28:58 +0200

    fix-for-all-posts-rendering

--- app/services/read_posts.rb
+++ app/services/read_posts.rb
@@ -7,14 +7,11 @@ class ReadPostsService
 
   attr_reader :category_name, :category
 
-  def sort_by_category; @posts = category.posts; end
-
-
-  def sort_by_commit_date; @posts.sort_by {|obj| obj.created_at }; end
-
-  def posts_with_category; sort_by_category && sort_by_commit_date && @posts; end
-
-  def all_posts; (@posts = Post.all) && sort_by_commit_date && @posts; end
-
   def show_posts; category_name ? posts_with_category : all_posts; end
+
+  private
+  def posts_with_category; sort_by_category && sort_by_commit_date && @posts; end
+  def sort_by_category; @posts = category.posts; end
+  def sort_by_commit_date; @posts = @posts.sort_by {|obj| obj.created_at }.reverse; end
+  def all_posts; (@posts = Post.all) && sort_by_commit_date && @posts; end
 end
